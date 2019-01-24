fb1bd7f96775421654112e51ae51b8bc171624f3
Alejandro Guardiola <theskkiper@gmail.com>
2019-01-24 00:23:39 -0500

    added bitmap drawable

---
+++ OpenGui/Graphics/BitmapDrawable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SkiaSharp;
+
+namespace OpenGui.Graphics
+{
+    public class BitmapDrawable : Drawable
+    {
+        /// <summary>
+        /// Get the bitmap used to draw
+        /// </summary>
+        public SKBitmap Bitmap { get; private set; }
+
+        public BitmapDrawable(SKBitmap bitmap)
+        {
+            Bitmap = bitmap;
+        }
+
+        public override void Draw(int width, int height, SKCanvas canvas)
+        {
+            using (var paint = new SKPaint())
+            {
+                canvas.DrawBitmap(Bitmap, 0, 0, paint);
+            }
+                
+        }
+    }
+}
